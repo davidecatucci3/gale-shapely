806686626734395be6cd1f5481c3bf0dda3f2eae
Your Name <you@example.com>
2025-03-08 18:12:22 +0100

    faster implementation but still no the fastest

--- Main.java
+++ Main.java
@@ -1,33 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     private int[][] apref;
     private int[][] ranking;
     private int[] next;
-    private final int n1;
-    private int[][] matchings;
+    private final int n;
+    private int[] matchings;
 
     public Main(int[][] apref, int[][] ranking) {
-        // if the length of the two sets is not the same raise an error
-        n1 = apref.length;
-        int n2 = ranking.length;
+        this.n = apref.length;
 
-        if (n1 != n2) {
+        // if the length of the two sets is not the same raise an error
+        if (n != ranking.length) {
             System.out.println("Error: Lengths of arrays do not match");
         } else {
             this.apref = apref;
             this.ranking = ranking;
-            this.next = new int[n1];
-            this.matchings = new int[n1][(n1 * 2) / apref[0].length];
+            this.next = new int[n];
+            this.matchings = new int[n];
 
-            // initialize all matches to unmatched (-1)
-            for (int i = 0; i < n1; i++) {
-                matchings[i] = new int[] {i, -1};
-            }
+            // initialize all matches to unmatched
+            Arrays.fill(matchings, -1);
         }
     }
 
     public boolean is_all_matched() {
-        for (int i = 0; i < n1; i++) {
-            if (matchings[i][1] == -1) {
+        // check if there is an ai that is not matched in that case return false
+        for (int i = 0; i < n; i++) {
+            if (matchings[i] == -1) {
                 return false;
             }
         }
@@ -38,38 +38,38 @@ public class Main {
     public int[][] run() {
         // until there are still matching to do continue
         while (!is_all_matched()) {
-            // each ai that is unmatched makes proposal to his best preference
-            int j = (n1 * 2) / apref[0].length;
-            int[][] curr_matchings = new int[n1][j];
+            int j = (n * 2) / apref[0].length;
+            int[][] curr_matchings = new int[n][j];
 
-            for (int i = 0; i < n1; i++) {
-                if (matchings[i][1] == -1) { // if ai is unmatched
+            // each bi accept or refuse the ai proposal based on the rankings array
+            for (int i = 0; i < n; i++) {
+                if (matchings[i] == -1) { // if ai is unmatched
                     int k = next[i];
 
                     curr_matchings[i] = new int[]{i, apref[i][k] - 1};
                 } else {
-                    curr_matchings[i] = new int[]{-1, -1};
+                    curr_matchings[i] = new int[]{i, -1};
                 }
             }
 
             // each bi accept or refuse the ai proposal based on the rankings array
-            for (int i = 0; i < n1; i++) {
-                if (curr_matchings[i][0] != -1) { // if ai is unmatched
+            for (int i = 0; i < n; i++) {
+                if (curr_matchings[i][1] != -1) { // if there is a match
                     if (curr_matchings[i][0] == ranking[curr_matchings[i][1]][0] - 1) { // stable matching
-                        for (int r = 0; r < n1; r++) {
-                            if (matchings[r][1] == curr_matchings[i][1]) {
-                                matchings[r][1] = -1; // becomes free
+                        for (int r = 0; r < n; r++) {
+                            if (matchings[r] == curr_matchings[i][1]) {
+                                matchings[r] = -1; // becomes free
 
                                 next[i]++;
                             }
                         }
 
-                        matchings[i] = new int[]{i, curr_matchings[i][1]};
+                        matchings[i] = curr_matchings[i][1];
                     } else { // refuse
                         boolean refused = false;
 
-                        for (int r = 0; r < n1; r++) {
-                            if (matchings[r][1] == curr_matchings[i][1]) {
+                        for (int r = 0; r < n; r++) {
+                            if (matchings[r] == curr_matchings[i][1]) {
                                 refused = true;
 
                                 next[i]++;
@@ -77,14 +77,23 @@ public class Main {
                         }
 
                         if (!refused) {
-                            matchings[i] = new int[]{i, curr_matchings[i][1]};
+                            matchings[i] = curr_matchings[i][1];
                         }
                     }
                 }
             }
         }
 
-        return matchings;
+        // format results as 1-based indices, its not part of the algorithm you can choose if use it or not
+        int j = (n * 2) / apref[0].length;
+        int[][] results = new int[n][j];
+
+        for (int i = 0; i < n; i++) {
+            results[i][0] = i + 1;
+            results[i][1] = matchings[i] + 1;
+        }
+
+        return results;
     }
 }
 
--- TestMain.java
+++ TestMain.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class TestMain {
     public static void main(String[] args) {
-        int[][] apref = {{2, 4, 1, 3}, {1, 3, 4, 2}, {1, 3, 2, 4}, {2, 1, 4, 3}};
-        int[][] ranking =  {{1, 4, 2, 3}, {4, 2, 1, 3}, {1, 4, 3, 2}, {2, 4, 1, 3}};
+        int[][] apref = {{4, 1, 2, 3}, {3, 1, 4, 2}, {3, 2, 4, 1}, {3, 2, 1, 4}};
+        int[][] ranking =  {{2, 3, 4, 1}, {3, 1, 4, 2}, {2, 4, 3, 1}, {1, 4, 2, 3}};
 
         Main algo = new Main(apref, ranking);
 
